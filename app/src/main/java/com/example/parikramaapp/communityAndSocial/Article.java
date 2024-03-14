bf950100f58fb5fb6590c2269d94e2ca581aa86e
Reyansh Gupta <happygupta@aol.in>
2024-03-15 00:42:16 +0530

    news try v3

---
+++ app/src/main/java/com/example/parikramaapp/communityAndSocial/Article.java
@@ -0,0 +1,48 @@
+package com.example.parikramaapp.communityAndSocial;
+
+public class Article {
+    private String title;
+    private String description;
+    private String imageUrl;
+    private String publishedAt;
+
+    public Article(String title, String description, String imageUrl, String publishedAt, String at) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.publishedAt = publishedAt;
+    }
+
+    // Getters and setters for all fields
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+}
