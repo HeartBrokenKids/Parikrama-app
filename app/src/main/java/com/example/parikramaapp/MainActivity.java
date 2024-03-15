4b1ea4d980d17249376313f5a56f77b2b4032778
Bhavya Chopra <64201249+bhavyachopra99@users.noreply.github.com>
2024-03-15 10:30:56 +0530

    gg_pfa
    Co-Authored-By: hittanshu <91820006+hittanshu@users.noreply.github.com>

--- app/src/main/java/com/example/parikramaapp/MainActivity.java
+++ app/src/main/java/com/example/parikramaapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.parikramaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.parikramaapp.communityAndSocial.CommunityMainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create instance of your fragment
+        CommunityMainFragment communityFragment = new CommunityMainFragment();
+
+        // Get fragment manager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Start a transaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the content of the container with your fragment
+        fragmentTransaction.replace(R.id.fragment_container, communityFragment);
+
+        // Commit the transaction
+        fragmentTransaction.commit();
     }
 }
